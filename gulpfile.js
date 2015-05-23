00db68713f7847404001d5dfeb4f723e89944862
jerrysdesign <jerry7722@hotmail.com>
2015-05-23 12:39:20 +0800

    整理gulpfile 路徑變數

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,101 @@
-var gulp = require('gulp'), 
-	slim = require('gulp-slim'),
-	sass = require('gulp-ruby-sass') ,
-	notify = require("gulp-notify") ,
-	bower = require('gulp-bower'),
-	webserver = require('gulp-webserver'),
-	connect = require('gulp-connect');
+
+var gulp      = require('gulp'), 
+    slim      = require('gulp-slim'),
+    sass      = require('gulp-ruby-sass') ,
+    notify    = require("gulp-notify") ,
+    bower     = require('gulp-bower'),
+    webserver = require('gulp-webserver'),
+    connect   = require('gulp-connect');
+
+// Sources config
+var source = {
+  resource : './resources',
+  slimPath : './resources/slim',
+  sassPath : './resources/sass',
+  bowerDir : './bower_components' 
+}
 
 var config = {
-	slimPath: './resources/slim',
-	sassPath: './resources/sass',
-	bowerDir: './bower_components' 
+  templates: {
+    main: [source.slimPath + '/index.slim'],
+    file: [source.slimPath + '/*.slim'],
+    watch: [source.slimPath + '/*.slim']
+  },
+  styles: {
+    main: [source.sassPath + '/style.scss'],
+    file: [source.sassPath + '/*.scss'],
+    watch: [source.sassPath + '/*.scss']
+  },
+  dest: {
+    templates: './public',
+    styles: './public/css'
+  },
+  server: './public'
+}
+
+var buide = {
+  templates: config.templates.file,
+  styles: config.styles.main
 }
 
 gulp.task('bower', function() { 
-	return bower()
-		.pipe(gulp.dest(config.bowerDir)) 
+  return bower()
+    .pipe(gulp.dest(source.bowerDir)) 
 });
 
 gulp.task('icons', function() { 
-	return gulp.src(config.bowerDir + '/fontawesome/fonts/**.*') 
-		.pipe(gulp.dest('./public/fonts')); 
+  return gulp.src(source.bowerDir + '/fontawesome/fonts/**.*') 
+    .pipe(gulp.dest('./public/fonts')); 
 });
 
-gulp.task('slim', function(){
-	gulp.src(config.slimPath + '/*.slim')
-		.pipe(slim({
-			pretty: true
-		}))
-		.pipe(gulp.dest("./public"));
+gulp.task('templates', function() {
+  gulp.src(config.templates.file)
+    .pipe(slim({
+      pretty: true
+    }))
+    .pipe(gulp.dest(config.dest.templates))
 });
 
 gulp.task('css', function() { 
-	return gulp.src(config.sassPath + '/style.scss')
-		.pipe(sass({
-			style: 'compressed',
-			loadPath: [
-				'./resources/sass',
-				config.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
-				config.bowerDir + '/fontawesome/scss',
-			]
-		 }) 
-			.on("error", notify.onError(function (error) {
-				return "Error: " + error.message;
-			}))) 
-		.pipe(gulp.dest('./public/css'))
-		.pipe(connect.reload()); 
+  return gulp.src(config.styles.main)
+    .pipe(sass({
+      style: 'compact',
+      loadPath: [
+        source.sassPath,
+        source.bowerDir + '/bootstrap-sass-official/assets/stylesheets',
+        source.bowerDir + '/fontawesome/scss'
+        ]
+     }) 
+    .on("error", notify.onError(function (error) {
+      return "Error: " + error.message;
+    }))) 
+    .pipe(gulp.dest(config.dest.styles))
+    .pipe(connect.reload()); 
 });
 
 gulp.task('webserver', function() {
-	gulp.src('./public')
-		.pipe(webserver({
-			host: 'localhost',
-			port: '5000',
-			livereload: true,
-			directoryListing: false
-	}));
-});
-// Rerun the task when a file changes
- gulp.task('watch', function() {
-	gulp.watch(config.sassPath + '/*.scss', ['css']); 
+  gulp.src(config.server)
+    .pipe(webserver({
+      host: 'localhost',
+      port: '5000',
+      livereload: true,
+      directoryListing: false
+  }));
 });
 
-gulp.task('default', ['bower', 'icons', 'slim', 'css', 'webserver', 'watch']);
+// Rerun the task when a file changes
+ gulp.task('watch', function() {
+  gulp.watch(config.styles.watch, ['css']); 
+  gulp.watch(config.templates.watch, ['templates']); 
+});
+
+var tasks = [
+  'bower',
+  'icons',
+  'templates',
+  'css',
+  'webserver',
+  'watch'
+];
+
+gulp.task('default', tasks);
